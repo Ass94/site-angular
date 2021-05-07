a82d0998c1780d7fe8c3871008f2ab84779bf9f4
Alex <alex94tu@gmail.com>
2021-05-07 10:58:11 -0300

    Dúvidas frequentes

--- src/app/duvidas/duvidas.component.ts
+++ src/app/duvidas/duvidas.component.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DuvidasComponent implements OnInit {
 
+  exibir1: boolean = false;
+  exibir2: boolean = false;
+  exibir3: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  exibirConteudop1(): void {
+    if (this.exibir1 == false) {
+      this.exibir1 = true;
+    } else {
+      this.exibir1 = false;
+    }
+    
+  }
+
+  exibirConteudop2(): void {
+    if (this.exibir2 == false) {
+      this.exibir2 = true;
+    } else {
+      this.exibir2 = false;
+    }
+    
+  }
+
+  exibirConteudop3(): void {
+    if (this.exibir3 == false) {
+      this.exibir3 = true;
+    } else {
+      this.exibir3 = false;
+    }
+    
+  }
+
+
 
 }
